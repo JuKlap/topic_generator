8ea35c589b7fc621077b892bfd3417dda499a6dc
Juste Klapatauskaite <klapatauskaite.juste@accenture.com>
2022-10-28 15:45:28 +0300

    Initial commit

---
+++ src/main/java/techxploration/topic_generator/TopicGeneratorApplication.java
@@ -0,0 +1,13 @@
+package techxploration.topic_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techxploration/topic_generator/TopicGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package techxploration.topic_generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
